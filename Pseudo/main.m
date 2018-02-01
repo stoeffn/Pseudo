62cd9a936c451a5d8ae14d4cca6d5224d4d99d3c
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-02-01 20:32:10 +0100

    Initial Commit

---
+++ Pseudo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pseudo
+//
+//  Created by Steffen Ryll on 01.02.18.
+//  Copyright © 2018 Steffen Ryll. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
