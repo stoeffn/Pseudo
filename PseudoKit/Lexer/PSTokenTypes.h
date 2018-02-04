c2262eea7de55baffa633444f2c4cf28b9b9d273
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-02-04 13:58:30 +0100

    Extend PSTokenTypes to include all token types

---
+++ PseudoKit/Lexer/PSTokenTypes.h
@@ -0,0 +1,64 @@
+//
+//  PSTokenTypes.h
+//  Pseudo
+//
+//  Created by Steffen Ryll on 01.02.18.
+//  Copyright © 2018 Steffen Ryll. All rights reserved.
+//
+
+typedef NS_ENUM(NSInteger, PSTokenTypes) {
+    PSTokenTypesComma,
+    PSTokenTypesColor,
+    PSTokenTypesSemicolon,
+
+    PSTokenTypesPoint,
+    PSTokenTypesOpeningParenthesis,
+    PSTokenTypesClosingParanthesis,
+    PSTokenTypesOpeningBracket,
+    PSTokenTypesClosingBracket,
+
+    PSTokenTypesPlus,
+    PSTokenTypesMinus,
+    PSTokenTypesTimes,
+    PSTokenTypesDividedBy,
+    PSTokenTypesDividedAsIntegerBy,
+    PSTokenTypesModulo,
+
+    PSTokenTypesEquals,
+    PSTokenTypesNotEquals,
+    PSTokenTypesGreaterThan,
+    PSTokenTypesGreaterThanOrEquals,
+    PSTokenTypesLessThan,
+    PSTokenTypesLessThanOrEquals,
+
+    PSTokenTypesAssign,
+
+    PSTokenTypesNot,
+
+    PSTokenTypesIf,
+    PSTokenTypesThen,
+    PSTokenTypesElse,
+
+    PSTokenTypesFor,
+    PSTokenTypesIn,
+    PSTokenTypesTo,
+    PSTokenTypesDownTo,
+    PSTokenTypesDo,
+    PSTokenTypesWhile,
+    PSTokenTypesRepeat,
+    PSTokenTypesUntil,
+    PSTokenTypesBreak,
+    PSTokenTypesContinue,
+
+    PSTokenTypesAlgorithm,
+    PSTokenTypesReturn,
+
+    PSTokenTypesNew,
+
+    PSTokenTypesNull,
+    PSTokenTypesTrue,
+    PSTokenTypesFalse,
+    PSTokenTypesIdentifier,
+    PSTokenTypesNumber,
+    PSTokenTypesString
+};
