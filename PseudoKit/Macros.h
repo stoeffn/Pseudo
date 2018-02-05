36985e1e35a3e243f1f990c9aceb599ca7ca3e44
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-02-05 12:59:21 +0100

    Add set literal macro

---
+++ PseudoKit/Macros.h
@@ -0,0 +1,9 @@
+//
+//  Macros.h
+//  Pseudo
+//
+//  Created by Steffen Ryll on 05.02.18.
+//  Copyright © 2018 Steffen Ryll. All rights reserved.
+//
+
+#define $set(...) [NSSet setWithArray: @[__VA_ARGS__]]
