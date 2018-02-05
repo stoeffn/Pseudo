1e277b42775ae07071861acc7a9894ef6bc38d88
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-02-05 16:30:40 +0100

    Use REPL in main program

--- Pseudo/main.m
+++ Pseudo/main.m
@@ -7,29 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <JavaScriptCore/JavaScriptCore.h>
 #import "PseudoKit.h"
 
 int main(int argc, const char *argv[]) {
     @autoreleasepool {
-        NSString *code = @"1 + 1";
-
-        PSStringReader *reader = [[PSStringReader alloc] initWithString: code];
-        PSLexer *lexer = [[PSLexer alloc] initWithReader: reader];
-        PSParser *parser = [[PSParser alloc] initWithLexer: lexer];
-
-        NSError *error;
-        PSNode *node = [parser expressionWithError: &error];
-
-        if (error) {
-            NSLog(@"Error: %@", error.localizedDescription);
-            return (int) error.code;
-        }
-
-        PSJavaScriptTranspiler *javaScriptTranspiler = [[PSJavaScriptTranspiler alloc] initWithNode: node];
-        JSContext *context = [[JSContext alloc] init];
-        JSValue *value = [context evaluateScript: javaScriptTranspiler.code];
-        NSLog(@">>> %@", value);
+        PSREPL *repl = [[PSREPL alloc] initWithInterpreter: [[PSJavaScriptInterpreter alloc] init]];
+        [repl enter];
     }
     return 0;
 }
