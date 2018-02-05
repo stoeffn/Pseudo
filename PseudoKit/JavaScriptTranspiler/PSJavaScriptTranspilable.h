f17959d7bfad8302401c0c834b871e608293faba
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-02-05 14:20:09 +0100

    Remove old syntax nodes

--- PseudoKit/JavaScriptTranspiler/PSJavaScriptTranspilable.h
+++ PseudoKit/JavaScriptTranspiler/PSJavaScriptTranspilable.h
@@ -6,13 +6,8 @@
 //  Copyright © 2018 Steffen Ryll. All rights reserved.
 //
 
-#ifndef PSJavaScriptTranspilable_h
-#define PSJavaScriptTranspilable_h
-
 @protocol PSJavaScriptTranspilable <NSObject>
 
 - (NSString *) javaScriptCode;
 
 @end
-
-#endif /* PSJavaScriptTranspilable_h */
